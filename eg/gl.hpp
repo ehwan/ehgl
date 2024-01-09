2d780911b1612ac17414075c1de891a314101989
Taehwan Kim <ehwank98@gmail.com>
2024-01-09 22:31:02 +0900

    shaders to source file

--- eg/gl.hpp
+++ eg/gl.hpp
@@ -41,8 +41,6 @@
 
 namespace eg {
 
-void throw_exception( int err );
-
 class exception
   : public std::exception
 {
@@ -57,8 +55,31 @@ public:
   {
     return err_;
   }
-  const char* what() const noexcept override;
+  const char* what() const noexcept override
+  {
+    switch( err_ )
+    {
+    case GL_NO_ERROR:
+      return "GL_NO_ERROR";
+    case GL_INVALID_ENUM:
+      return "GL_INVALID_ENUM";
+    case GL_INVALID_VALUE:
+      return "GL_INVALID_VALUE";
+    case GL_INVALID_OPERATION:
+      return "GL_INVALID_OPERATION";
+    case GL_OUT_OF_MEMORY:
+      return "GL_OUT_OF_MEMORY";
+    default:
+      return "UNKNOWN_GL_ERROR";
+    }
+  }
+
 };
+
+static inline void throw_exception( int err )
+{
+  throw eg::exception( err );
+}
 
 }
 
--- example/mandelbrot/mandelbrot.cpp
+++ example/mandelbrot/mandelbrot.cpp
@@ -1,17 +1,71 @@
-// compile arguments for OSX :
-// -lsfml-window -lsfml-graphics -lsfml-system -framework OpenGL
-//
 // MANDELBROT Fractals Fragment shader
 
 #define GL_SILENCE_DEPRECATION
-#include "../../eg.hpp"
+// place your opengl header in <eg/gl.hpp>
+#include <eg.hpp>
 #include <vector>
 #include <fstream>
+
+
+const char* vertex_shader =
+"#version 330 core\n"
+"\n"
+"void main(){\n"
+  "const vec2 vertices[4] = vec2[](\n"
+    "vec2(-1.0,-1.0),\n"
+    "vec2(+1.0,-1.0),\n"
+    "vec2(-1.0,+1.0),\n"
+    "vec2(+1.0,+1.0)\n"
+  ");\n"
+  "gl_Position = vec4( vertices[gl_VertexID], 0.0, 1.0 );\n"
+"}\n";
+const char *fragment_shader =
+"#version 330 core\n"
+"out vec4 outcol;\n"
+"\n"
+"const float M_PI = 3.141592;\n"
+"vec3 colormap( float x )\n"
+"{\n"
+  "vec3 factor = vec3(\n"
+    "clamp( cos( (x-0.25)*M_PI/0.3 ), 0.0, 1.0 ),\n"
+    "clamp( cos( (x-0.50)*M_PI/0.6 ), 0.0, 1.0 ),\n"
+    "clamp( cos( (x-0.75)*M_PI/0.5 ), 0.0, 1.0 )\n"
+  ");\n"
+  "return factor;\n"
+  "//return vec3(x);\n"
+"}\n"
+"\n"
+"// z_{n+1} = (z_n)^2 + c\n"
+"vec2 iterate( vec2 z, vec2 c )\n"
+"{\n"
+"vec2 z2 = vec2( z.x*z.x - z.y*z.y, 2.0*z.x*z.y );\n"
+"return z2 + c;\n"
+"}\n"
+"float threshold_sq = 4.0;\n"
+"const int MAXIT = 100;\n"
+"// return iteration count\n"
+"int mandelbrot_iterate( vec2 c )\n"
+"{\n"
+	"vec2 z = vec2(0.0);\n"
+	"for( int it=0; it<MAXIT; ++it )\n"
+	"{\n"
+		"if( dot(z,z) > threshold_sq ){ return it; }\n"
+		"z = iterate( z, c );\n"
+	"}\n"
+	"return MAXIT;\n"
+"}\n"
+"void main( void ) {\n"
+	"vec2 scale_factor = vec2( 4.0 );\n"
+	"vec2 position = (( gl_FragCoord.xy / vec2(800.0,800.0) )-vec2(0.5))*scale_factor;\n"
+	"int i = mandelbrot_iterate( position );\n"
+"\n"
+	"outcol = vec4( colormap(float(i)/float(MAXIT)), 1.0);\n"
+"\n"
+"}\n";
 
 eg::Program program;
 eg::VertexArray varray;
 eg::debug::window_context window;
-
 
 // event callback function
 void event( eg::debug::window_context &w )
@@ -45,8 +99,14 @@ int main()
 
   // program creation & linkage
   {
-    auto vertex = eg::load_shader( "../example/mandelbrot/vert.glsl", GL_VERTEX_SHADER );
-    auto fragment = eg::load_shader( "../example/mandelbrot/frag.glsl", GL_FRAGMENT_SHADER );
+    auto vertex = eg::make_shader( GL_VERTEX_SHADER );
+    vertex.source( vertex_shader );
+    vertex.compile();
+
+    auto fragment = eg::make_shader( GL_FRAGMENT_SHADER );
+    fragment.source( fragment_shader );
+    fragment.compile();
+
     program = eg::make_program();
     program.attachShader( vertex );
     program.attachShader( fragment );
